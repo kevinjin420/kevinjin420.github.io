1ce4bcbc2cba204dea2e32e5cfbdf67510f498bd
Kevinj <kevinjin0420@gmail.com>
2024-12-05 01:15:58 -0500

    title font fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,12 @@ import { Outlet, Link } from 'react-router-dom';
 function Navbar() {
   return (
     <>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Italianno&display=swap');
+    </style>    
       <nav className="navbar">
         <h1 className='signature'>
-        <Link to="/"> Shengran (Kevin) Jin </Link>
+        <Link to="/"> Kevin Jin </Link>
         </h1>
         <div className="flex">
           <Link to="/"> Home </Link>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import '../App.css'
 import portrait from '../assets/portrait.png';
 import banner from '../assets/sky.png';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function About() {
 
@@ -37,7 +38,9 @@ function About() {
             College of Engineering, double majoring in Computer Engineering and Data Science. 
           </p>
         </div>
-      </div>
+      </div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-strava" viewBox="0 0 16 16">
+        <path d="M6.731 0 2 9.125h2.788L6.73 5.497l1.93 3.628h2.766zm4.694 9.125-1.372 2.756L8.66 9.125H6.547L10.053 16l3.484-6.875z"/>
+      </svg>
 
       {/* Photos Section */}
       <div style={{ margin: '0 20px 40px' }}>
