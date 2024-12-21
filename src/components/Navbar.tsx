c744a146db2a698e3f91c88050545c0398881327
Kevinj <kevinjin0420@gmail.com>
2024-12-21 14:43:15 -0500

    fixed link shrinking

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
 // import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Italianno&display=swap');
-    </style>    
+    </style>
       <nav className="navbar">
-        <h1 className='signature'>
-        <Link to="/"> Kevin Jin </Link>
+        <h1 className="signature">
+          <Link to="/"> Kevin Jin </Link>
         </h1>
-        <div className="flex">
-          <Link to="/"> Home </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/projects"> Projects </Link>
-          <Link to="/resume"> Resume </Link>
-          <Link to="/contact"> Contact </Link>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <div className={`links ${menuOpen ? 'open' : ''}`}>
+          <Link to="/" onClick={() => setMenuOpen(false)}> Home </Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}> About </Link>
+          <Link to="/projects" onClick={() => setMenuOpen(false)}> Projects </Link>
+          <Link to="/resume" onClick={() => setMenuOpen(false)}> Resume </Link>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}> Contact </Link>
         </div>
       </nav>
       <Outlet />
