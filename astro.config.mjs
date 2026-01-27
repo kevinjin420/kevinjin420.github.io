d581849b4e004c1e9de6152916979d1b088fbfba
kevinjin420 <kevinjin0420@gmail.com>
2026-01-27 11:42:33 -0500

    ci deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://kevinjin420.github.io',
+  site: 'https://kevinjin.dev',
   base: '/',
   outDir: './dist',
   publicDir: './public',
