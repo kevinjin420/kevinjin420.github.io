df3504d36dbd63fe67970492147c9e3a309425cd
kevinjin420 <kevinjin0420@gmail.com>
2026-02-11 15:56:18 -0500

    page detection

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,14 @@
 
 export default new Map([
+["src/content/docs/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fabout.mdx&astroContentModuleFlag=true")],
+["src/content/docs/links.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Flinks.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/resume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fresume.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/blog/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fblog%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/projects/fpv-drone.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Ffpv-drone.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/projects/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/projects/mrover-gui.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Fmrover-gui.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/projects/jaseci-docbench.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Fjaseci-docbench.mdx&astroContentModuleFlag=true")],
+["src/content/docs/projects/jaseci-llmdocs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Fjaseci-llmdocs.mdx&astroContentModuleFlag=true")],
+["src/content/docs/projects/mrover-gui.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Fmrover-gui.mdx&astroContentModuleFlag=true")],
+["src/content/docs/projects/mrover-website.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fprojects%2Fmrover-website.mdx&astroContentModuleFlag=true")]]);
 		
